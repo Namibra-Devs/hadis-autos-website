add7685de711cf7ffe9cd2ebd54bfcddc9d05043
Kindo <user@USERs-MacBook-Pro.local>
2026-02-12 10:43:09 +0000

    browse section added

--- hadi-autos/src/pages/Home.jsx
+++ hadi-autos/src/pages/Home.jsx
@@ -4,14 +4,14 @@ import FeaturedVehicles from '@components/home/FeaturedVehicles'
 import HowItWorks from '@components/shared/HowItWorks'
 import Testimonials from '@components/shared/Testimonials'
 import CTASection from '@components/home/CTASection'
+import BrowseSection from '../components/home/BrowseSection'
 
 const Home = () => {
   return (
     <div className="relative">
       {/* Flipbook Hero Sections */}
       <FlipbookHero />
-      
-      {/* Rest of the sections follow naturally */}
+      <BrowseSection />
       <WhyChooseUs />
       <FeaturedVehicles />
       <HowItWorks />
